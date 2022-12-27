8d887f6e10472f68703801ebf31c3f9f1360c1bf
jona lippo <jonalippo@gmail.com>
2022-12-27 15:07:54 -0300

    Clase juegoAdivinaPar #8

--- app-services/src/main/java/com/jona/JuegoAdivinaPar.java
+++ app-services/src/main/java/com/jona/JuegoAdivinaPar.java
@@ -5,19 +5,21 @@ public class JuegoAdivinaPar extends JuegoAdivinaNumero {
 	public JuegoAdivinaPar(int nVidas, int numAdivinar) {
 		super(nVidas, numAdivinar);
 	}
-	
+
 	public boolean validaNumero(int numero) {
-		if (numero %2==0 && numero <=10 && numero >=0) {
+		if (numero % 2 == 0 && numero <= 10 && numero >= 0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	public String muestraNombre() {
 		return "Adivina un número par";
 	}
-	
 
+	public String muestraInfo() {
+		return "Instructivo";
+	}
 
 }
